85a2367f05f9d8abb57655aa6977718f82dab8be
dyau <dennisyau@gmail.com>
2022-02-11 20:55:48 +0000

    unittest methods MUST start with test_ !

--- python/test.py
+++ python/test.py
@@ -6,24 +6,24 @@ from return_spam_line import *
 
 class TestCounter(unittest.TestCase):
 
-    def get_counter0(self):
+    def test_get_counter0(self):
         self.assertEqual(get_counter("2022-01-01 00:00"), 0)
-    def create_counter0(self):
+    def test_create_counter0(self):
         self.create_test = create_counter("2022-01-01 00:00")
-    def increment_counter1(self):
+    def test_increment_counter1(self):
         self.increment_test = increment_counter("2022-01-01 00:00")
-    def get_counter1(self):
+    def test_get_counter1(self):
         self.assertEqual(get_counter("2022-01-01 00:00"), 1)
-    def increment_counter2(self):
+    def test_increment_counter2(self):
         self.increment_test = increment_counter("2022-01-01 00:00")
-    def get_counter2(self):
+    def test_get_counter2(self):
         self.assertEqual(get_counter("2022-01-01 00:00"), 2)
 
-    def return_spam0(self):
+    def test_return_spam0(self):
         self.assertEqual(return_spam_line(0), "Egg and Spam")
-    def return_spam1(self):
+    def test_return_spam1(self):
         self.assertEqual(return_spam_line(1), "Egg, bacon and Spam")
-    def return_spam8(self):
+    def test_return_spam8(self):
         self.assertEqual(return_spam_line(8), "Spam, Spam, Spam, Spam, Spam, Spam, Spam and Spam")
 
 if __name__ == '__main__':
