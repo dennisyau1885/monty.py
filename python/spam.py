0cc994a6380bf0d3886c0e11d9c5e88c9d9e3efc
dyau <dennisyau@gmail.com>
2022-02-12 23:39:06 +0000

    vscode format doc tabs2spaces

--- python/spam.py
+++ python/spam.py
@@ -5,9 +5,9 @@ from get_counter import *
 from return_spam_line import *
 
 if __name__ == '__main__':
-	datetime_ = datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M")
-	counter = get_counter(datetime_)
-	if counter == 0:
-		create_counter(datetime_)
-	print(return_spam_line(counter))
-	increment_counter(datetime_)
+    datetime_ = datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M")
+    counter = get_counter(datetime_)
+    if counter == 0:
+        create_counter(datetime_)
+    print(return_spam_line(counter))
+    increment_counter(datetime_)
